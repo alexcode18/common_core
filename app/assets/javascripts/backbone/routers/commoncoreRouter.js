564a279b1ed165e7b006425fc4dc94aaff255a71
alexcode18 <aschattner@gmail.com>
2015-02-11 14:39:35 -0500

    removed console log from router

--- app/assets/javascripts/backbone/routers/commoncoreRouter.js
+++ app/assets/javascripts/backbone/routers/commoncoreRouter.js
@@ -1,7 +1,5 @@
 App.Routers.Router = Backbone.Router.extend({
 	initialize: function() {
-		// App.collection = new App.Collections.BookCollection();
-		// App.modal = new App.Views.BookModalView();
 	},
 	routes: {
 		'':'index',
@@ -9,7 +7,6 @@ App.Routers.Router = Backbone.Router.extend({
 		'tags/:id':'tagView'
 	},
 	index: function() {
-		$(document.body).append('this route works!');
 	},
 	modalView: function(id) {
 		console.log(id);
@@ -19,7 +16,5 @@ App.Routers.Router = Backbone.Router.extend({
 		console.log('made it to tagView router: ' + App.tags.get(id));
 		var newView = new App.Views.TagView({model: App.tags.get(id)});
 		newView.getTagBooks();
-		// var tag = App.tags.get(id);
-		// tagBooks = new App.Views.BooksListView({collection: tag.get('books')});
 	}
 })
