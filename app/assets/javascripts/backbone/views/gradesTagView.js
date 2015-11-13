23996e5a558d56595832cf580d55f8dbf25af5ec
Alex Schattner <alexschattner@Alexs-MacBook-Pro.local>
2015-11-12 21:06:11 -0500

    added jPanel Menu  and bootstrap

---
+++ app/assets/javascripts/backbone/views/gradesTagView.js
@@ -0,0 +1,22 @@
+App.Views.GradesTagView = Backbone.View.extend({
+	tagName: 'button',
+	className: 'tag col-lg-12 col-md-12 col-sm-12 col-xs-12',
+	initialize: function() {
+		this.template = HandlebarsTemplates['listTag'];
+		this.renderTag();
+	},
+	events: {
+		'click':'getTagBooks'
+	},
+	renderTag: function() {
+		this.$el.html(this.template(this.model.toJSON()));
+	},
+	getTagBooks: function() {
+		App.bookModalView.hide();
+		$('html, body').animate({ scrollTop: 0 }, 0);
+		App.tagID = this.model.get('id');
+		App.books = new App.Collections.BookCollection();
+		App.books.fetchTagBooks();
+		App.router.navigate('tags/' + App.tagID);
+	}
+})
